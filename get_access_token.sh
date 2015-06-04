8fa5b6bc552b9cd5922e859874a9012aa8350997
Vadim <bavadim@gmail.com>
2015-06-04 16:47:43 +0300

    Update get_access_token.sh

--- get_access_token.sh
+++ get_access_token.sh
@@ -4,7 +4,7 @@ app_id='3483409'
 app_domain='http://oauth.vk.com/blank.html'
 
 
-open https://oauth.vk.com/authorize\?client_id\=${app_id}\&scope=offline,audio\&redirect_uri=${app_domain}\&display=page\&response_type=token
+open https://oauth.vk.com/authorize\?client_id\=${app_id}\&scope=offline,audio,messages,groups,wall,notes,docs,video,photos,friends\&redirect_uri=${app_domain}\&display=page\&response_type=token
 
 echo -n "Enter access_token from browser address string: "
 read access_token
